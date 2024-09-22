ab901a6b0cce5b97b6401de5b7d797d868cc8547
Alnoer <getmanovv950@gmail.com>
2024-09-22 16:36:56 +0300

    ime padding

--- app/src/main/java/dev/alnoer/flightsearch/ui/FlightSearchApp.kt
+++ app/src/main/java/dev/alnoer/flightsearch/ui/FlightSearchApp.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -83,6 +87,8 @@ fun FlightSearchApp(
             favoritesList = favoritesList,
             modifier = Modifier
                 .padding(innerPadding)
+                .consumeWindowInsets(WindowInsets.navigationBars)
+                .imePadding()
                 .fillMaxSize()
         )
     }
